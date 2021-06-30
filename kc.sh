a9e6da8860361520cd99d89fd50254439ea7cc05
chandracherukuri <42809378+chandracherukuri@users.noreply.github.com>
2021-06-30 08:42:34 +0530

    Update kc.sh

--- kc.sh
+++ kc.sh
@@ -38,4 +38,5 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
 kubectl get nodes
+kubectl taint nodes --all node-role.kubernetes.io/master-
 kubectl get pods --all-namespaces
