6cabf08276cc8dbeec74ee6ffb211f29f5c10bef
chandracherukuri <42809378+chandracherukuri@users.noreply.github.com>
2021-07-05 07:48:42 +0530

    Update kc.sh

--- kc.sh
+++ kc.sh
@@ -1,15 +1,11 @@
 #!/bin/bash/
 sudo apt-get update -y
 sudo apt-get install -y apt-transport-https
-
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-
 apt-get update -y
-
 swapoff -a
 sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
