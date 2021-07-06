c31f7c9af1eff24cc5327d733592c3fb3a321e70
chandracherukuri <42809378+chandracherukuri@users.noreply.github.com>
2021-07-06 07:29:06 +0530

    Update kc.sh

--- kc.sh
+++ kc.sh
@@ -8,31 +8,22 @@ EOF
 apt-get update -y
 swapoff -a
 sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
-
 modprobe br_netfilter
 sysctl -p
 sudo  sysctl net.bridge.bridge-nf-call-iptables=1
-
 apt install docker.io -y
-
 usermod -aG docker ubuntu
-
 systemctl restart docker
 systemctl enable docker.service
-
 apt-get install -y kubelet kubeadm kubectl kubernetes-cni
 systemctl daemon-reload
 systemctl start kubelet
 systemctl enable kubelet.service
-
 kubeadm init 
-
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
-
 kubectl get nodes
 kubectl taint nodes --all node-role.kubernetes.io/master-
 kubectl get pods --all-namespaces
